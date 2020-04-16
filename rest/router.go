905534b40f239073770b5570dc9c373eb92fb734
keitam <keitam913@yahoo.co.jp>
2020-04-16 18:45:06 +0900

    Serve stylesheets

--- rest/router.go
+++ rest/router.go
@@ -6,6 +6,8 @@ func NewRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.StaticFile("/main.js", "/usr/share/agility/assets/main.js")
+	r.StaticFile("/main.css", "/usr/share/agility/assets/main.css")
+
 	r.NoRoute(func(ctx *gin.Context) {
 		ctx.File("/usr/share/agility/assets/index.html")
 	})
