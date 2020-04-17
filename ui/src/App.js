a397766bd75d1ed770d9afbfd093707f3d06914b
keitam <keitam913@yahoo.co.jp>
2020-04-17 21:52:38 +0900

    Load the sprints

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,35 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-        return (
-                <div className="app">
+        let [sprints, setSprints] = useState([]);
+
+        function loadSprints(f) {
+                fetch('/v1/sprints').then((res) => {
+                        res.json().then((ss) => {
+                                f(ss);
+                        });
+                });
+        }
+
+        useEffect(() => {
+                loadSprints((ss) => {
+                        setSprints(ss);
+                });
+        }, []);
+
+        function metricTable(team) {
+                return (
+                        <div>
+                                <style jsx>{`
+                                        h2 {
+                                                color: #666;
+                                                font-size: large;
+                                        }
+
+                                        table {
+                                                border-width: 1pt 0 0 1pt;
+                                                border-color: #ccc;
+                                                border-style: solid;
+                                                border-collapse: collapse;
+                                                margin-bottom: 6ex;
+                                        }
+                                `}</style>
+                                <h2>{team}</h2>
+                                <table>
+                                        <thead>
+                                                {headerRow()}
+                                        </thead>
+                                        <tbody>
+                                                {metricRow(team, 'Commitment')}
+                                                {metricRow(team, 'Done')}
+                                                {metricRow(team, 'Velocity')}
+                                        </tbody>
+                                </table>
+                        </div>
+                );
+        }
+
+        function headerRow() {
+                return (<tr>
                         <style jsx>{`
-                                .tables {
-                                        margin: 1rem auto;
-                                        width: 900pt;
-                                }
-
-                                h1 {
-                                        background: #066;
-                                        margin: 0 0 2rem;
-                                        padding: 1rem;
-                                        color: #fff;
-                                        font-size: large;
-                                        font-weight: normal;
-                                }
-
-                                h2 {
-                                        color: #666;
-                                        font-size: large;
-                                }
-
-                                table {
-                                        border-width: 1pt 0 0 1pt;
+                                th {
+                                        border-width: 0 1pt 1pt 0;
                                         border-color: #ccc;
                                         border-style: solid;
-                                        border-collapse: collapse;
+                                        padding: 0.5ex;
+                                        text-align: left;
                                 }
 
+                                .metric-category {
+                                        width: 8em;
+                                }
+
+                                .sprint {
+                                        width: 5em;
+
+                                }
+                        `}</style>
+                        <th className="metric-category"></th>
+                        {sprints.map((s, i) => {
+                                return <th className="sprint" key={i}>{s.sprint}</th>;
+                        })}
+                </tr>);
+        }
+
+        function metricRow(team, category) {
+                return (<tr>
+                        <style jsx>{`
                                 th {
                                         border-width: 0 1pt 1pt 0;
                                         border-color: #ccc;
@@ -45,103 +94,36 @@ function App() {
                                         padding: 0.5ex;
                                         text-align: right;
                                 }
+                        `}</style>
+                        <th>{category}</th>
+                        {sprints.map((s, i) => {
+                                return <td key={i}>{s.teams[team][category.toLowerCase()]}</td>;
+                        })}
+                </tr>);
+        }
 
-                                .metric-category {
-                                        width: 8em;
+        return (
+                <div className="app">
+                        <style jsx>{`
+                                .tables {
+                                        margin: 1rem auto;
+                                        width: 800pt;
                                 }
 
-                                .sprint {
-                                        width: 5em;
-
-                                }
-
-                                .metric-table {
-                                        margin-bottom: 6ex;
+                                h1 {
+                                        background: #066;
+                                        margin: 0 0 2rem;
+                                        padding: 1rem;
+                                        color: #fff;
+                                        font-size: large;
+                                        font-weight: normal;
                                 }
                         `}</style>
                         <h1>Agility</h1>
                         <div className="tables">
-                                <h2>All</h2>
-                                <table className="metric-table">
-                                        <thead>
-                                                <tr>
-                                                        <th className="metric-category"></th>
-                                                        <th className="sprint">s11</th>
-                                                        <th className="sprint">s12</th>
-                                                </tr>
-                                        </thead>
-                                        <tbody>
-                                                <tr>
-                                                        <th>Commitment</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                                <tr>
-                                                        <th>Done</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                                <tr>
-                                                        <th>Velocity</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                        </tbody>
-                                </table>
-                                <h2>SRE 0</h2>
-                                <table className="metric-table">
-                                        <thead>
-                                                <tr>
-                                                        <th className="metric-category"></th>
-                                                        <th className="sprint">s11</th>
-                                                        <th className="sprint">s12</th>
-                                                </tr>
-                                        </thead>
-                                        <tbody>
-                                                <tr>
-                                                        <th>Commitment</th>
-                                                        <td>20 (5)</td>
-                                                        <td></td>
-                                                </tr>
-                                                <tr>
-                                                        <th>Done</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                                <tr>
-                                                        <th>Velocity</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                        </tbody>
-                                </table>
-                                <h2>SRE 1+2</h2>
-                                <table className="metric-table">
-                                        <thead>
-                                                <tr>
-                                                        <th className="metric-category"></th>
-                                                        <th className="sprint">s11</th>
-                                                        <th className="sprint">s12</th>
-                                                </tr>
-                                        </thead>
-                                        <tbody>
-                                                <tr>
-                                                        <th>Commitment</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                                <tr>
-                                                        <th>Done</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                                <tr>
-                                                        <th>Velocity</th>
-                                                        <td></td>
-                                                        <td></td>
-                                                </tr>
-                                        </tbody>
-                                </table>
+                                {metricTable('All')}
+                                {metricTable('SRE 0')}
+                                {metricTable('SRE 1+2')}
                         </div>
                 </div>
         );
