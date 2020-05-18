2d590e840a97b02df5bfa0813bb7d32e597e8a82
keitam <keitam913@yahoo.co.jp>
2020-05-18 22:39:25 +0900

    Add redis options

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,14 +12,21 @@ type Config struct {
 	JIRAUsername    string
 	JIRAPassword    string
 	JIRABoardID     string
+	RedisAddr       string
+	RedisPassword   string
+	RedisDB         int
 }
 
 func FromEnv() (Config, error) {
+	redisDB, _ := strconv.Atoi(os.Getenv("REDIS_DB"))
 	return Config{
 		Teams:           strings.Split(os.Getenv("TEAMS"), ","),
 		JIRAAPIEndpoint: os.Getenv("JIRA_API_ENDPOINT"),
 		JIRAUsername:    os.Getenv("JIRA_USERNAME"),
 		JIRAPassword:    os.Getenv("JIRA_PASSWORD"),
 		JIRABoardID:     os.Getenv("JIRA_BOARD_ID"),
+		RedisAddr:       os.Getenv("REDIS_ADDR"),
+		RedisPassword:   os.Getenv("REDIS_PASSWORD"),
+		RedisDB:         redisDB,
 	}, nil
 }
