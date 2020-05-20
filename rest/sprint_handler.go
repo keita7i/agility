c6c89f4f108fffda186932c2d1cd9a30557702ae
keitam <keitam913@yahoo.co.jp>
2020-05-20 22:11:12 +0900

    Fix velocities of early sprints

--- rest/sprint_handler.go
+++ rest/sprint_handler.go
@@ -8,31 +8,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const SHOW_SPRINTS = 8
+
 type SprintHandler struct {
 	ApplicationService *application.Service
 	Teams              []string
 }
 
 func (sh *SprintHandler) GET(ctx *gin.Context) {
-	sps, err := sh.ApplicationService.LastSprints(8)
+	sps, err := sh.ApplicationService.LastSprints(SHOW_SPRINTS + 2) // Get more sprints to calculate velocities
 	if err != nil {
 		panic(err)
 	}
 	srs := make([]SprintResponse, 0)
-	for i, sp := range sps {
+	for i := 0; i < len(sps) && i < SHOW_SPRINTS; i++ {
 		sr := SprintResponse{}
-		sr.Sprint = sp.Sprint()
+		sr.Sprint = sps[i].Sprint()
 		sr.Teams = make(map[string]TeamMetrics, 0)
 		sr.Teams["All"] = TeamMetrics{
-			Commitment: sp.AllCommitment(),
-			Done:       sp.AllDone(),
-			Velocity:   sp.AllVelocity(sps[i+1:]),
+			Commitment: sps[i].AllCommitment(),
+			Done:       sps[i].AllDone(),
+			Velocity:   sps[i].AllVelocity(sps[i+1:]),
 		}
 		for _, t := range sh.Teams {
 			sr.Teams[t] = TeamMetrics{
-				Commitment: sp.Commitment(t),
-				Done:       sp.Done(t),
-				Velocity:   sp.Velocity(t, sps[i+1:]),
+				Commitment: sps[i].Commitment(t),
+				Done:       sps[i].Done(t),
+				Velocity:   sps[i].Velocity(t, sps[i+1:]),
 			}
 		}
 		srs = append(srs, sr)
