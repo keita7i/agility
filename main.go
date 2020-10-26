b83bc7870d876e7ed091ff9dfb145ed353f3358d
Keita Mizuochi <keitam913@yahoo.co.jp>
2020-10-26 12:14:49 +0900

    Implement the prom handler

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
 	di := DI{}
-	panic(di.Router().Run(":80"))
+	panic(di.Router().Run(fmt.Sprintf(":%s", port)))
 }
--- rest/router.go
+++ rest/router.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func NewRouter(sh *SprintHandler, th *TeamHandler, bh *BoardHandler) *gin.Engine {
@@ -10,6 +11,11 @@ func NewRouter(sh *SprintHandler, th *TeamHandler, bh *BoardHandler) *gin.Engine
 	r.GET("/v1/sprints", sh.GET)
 	r.GET("/v1/teams", th.GET)
 	r.GET("/v1/boards", bh.GET)
+
+	ph := promhttp.Handler()
+	r.GET("/metrics", func(ctx *gin.Context) {
+		ph.ServeHTTP(ctx.Writer, ctx.Request)
+	})
 
 	r.StaticFile("/main.js", "/usr/share/agility/assets/main.js")
 	r.StaticFile("/main.css", "/usr/share/agility/assets/main.css")
