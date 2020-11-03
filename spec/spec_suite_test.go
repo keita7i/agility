d61e42cd968aa43253207bd91b5880b877d7cc7d
Keita Mizuochi <keitam913@yahoo.co.jp>
2020-11-03 16:17:20 +0900

    Setup the spec suite

--- spec/spec_suite_test.go
+++ spec/spec_suite_test.go
@@ -3,11 +3,24 @@ package spec_test
 import (
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+)
+
+var (
+	GinDefaultWriter = gin.DefaultWriter
 )
 
 func TestSpec(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Spec Suite")
 }
+
+var _ = BeforeSuite(func() {
+	gin.DefaultWriter = GinkgoWriter
+})
+
+var _ = AfterSuite(func() {
+	gin.DefaultWriter = GinDefaultWriter
+})
